b63de731ee1223e26d4aa3c53eac776241f01f50
Brynimation <zcabbll@ucl.ac.uk>
2023-02-20 23:31:12 +0000

    gogo

--- Native Plugin Test/Assets/NativePluginCaller.cs
+++ Native Plugin Test/Assets/NativePluginCaller.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class NativePluginCaller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CommandBuffer commandBuffer;
+    #if UNITY_IPHONE && !UNITY_EDITOR
+    [DllImport ("__Internal")]
+    #else
+    [DllImport("Dll1")] //name of the dll project. This imports the Dll of that name, and allows us to access its code from C#
+    #endif
+
+    /*An IntPtr is an integer the same size as a pointer. You can use IntPtr
+     to store a pointer value in a non-pointer type.
+     GetRenderEventFunction() is the function defined in our c++ code that returns our callback function; the function that we want unity to call
+     during the render loop. The render loop is just the term used to describe
+    all the rendering operations that occur in a single frame.
+
+    We use the "extern" modifier to declare a method that is implemented 
+    externally.
+     */
+    private static extern IntPtr getEventFunction();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void DebugDelegate(string str); //Delegates are functions that can be passed as parameters. Any method that takes in a string and returns void can be instantiated as a DebugDelegate.
+
+    [DllImport("Dll1")]
+    public static extern void SetDebugFunction(IntPtr fp);
+    static void CallBackFunction(string str) 
     {
-        
+        Debug.Log(str);
+    }
+    private void Start()
+    {
+        DebugDelegate callback = new DebugDelegate(CallBackFunction);
+        /*Convert the callback delegate into a function pointer that can be 
+         used in unmanaged code (ie, our c++ code).*/
+        IntPtr intPtrDelegate = Marshal.GetFunctionPointerForDelegate(callback);
+        SetDebugFunction(intPtrDelegate);   
+
+        commandBuffer = new CommandBuffer();
+        commandBuffer.name = "Dll1";
+        Camera.main.AddCommandBuffer(CameraEvent.AfterGBuffer, commandBuffer);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Queue a specific callback to be called on the render thread
+    private void Update()
     {
-        
+       commandBuffer.IssuePluginEvent(getEventFunction(), 0);
     }
+    
 }
+
+
--- Native Plugin Test/PluginSourceDll/Dll1/dllmain.cpp
+++ Native Plugin Test/PluginSourceDll/Dll1/dllmain.cpp
@@ -1,38 +1,38 @@
 #include <stdlib.h>
+#include "pch.h"
 #include <string>
 #include <iostream>
 #include "IUnityInterface.h"
 #include "IUnityGraphics.h"
-
+#include "d3d11.h" //direct113d header
+#include "IUnityGraphicsD3D11.h" //specific unity graphics api
 /*We use the IUnityGraphics interface to give our plugin access to generic graphics device
 functionality.
 
-To declare an array in c++: type arrName[numItems]
-type arrName[5] = {1,2,3,4,5} //can't contain more than 5 elements due to the size constraint
+UNITY_INTERFACE_EXPORT is a macro, defined as _declspec(dllexport). This allows
+us to export data, functions, classes and class members from a dll.
+UNITY_INTERFACE_API is a macro, defined as _stdcall. This is a calling convention
+used to call Win32 API functions. 
+A calling convention is an implementation-level scheme for how subroutines receive
+parameters from, and return results to, their caller.
+Any functions we want to be able to call from our C# script should be preceded by
+these two macros.
 */
+
+typedef void (*FuncPtr)(const char*);
+FuncPtr Debug;
+
+
 static IUnityInterfaces* s_UnityInterfaces = NULL;
 static IUnityGraphics* s_Graphics = NULL;
 static UnityGfxRenderer s_RendererType = kUnityGfxRendererNull;
 
-// Unity plugin load event
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
-UnityPluginLoad(IUnityInterfaces * unityInterfaces)
+ID3D11Device* device = NULL;
+ID3D11DeviceContext* context = NULL;
+
+extern "C" UNITY_INTERFACE_EXPORT void SetDebugFunction(FuncPtr fp)
 {
-    s_UnityInterfaces = unityInterfaces;
-    s_Graphics = unityInterfaces->Get<IUnityGraphics>();
-
-    s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
-
-    // Run OnGraphicsDeviceEvent(initialize) manually on plugin load
-    // to not miss the event in case the graphics device is already initialized
-    OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
-}
-
-// Unity plugin unload event
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
-UnityPluginUnload()
-{
-    s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
+    Debug = fp;
 }
 
 static void UNITY_INTERFACE_API
@@ -63,4 +63,37 @@ OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType)
         break;
     }
     };
+}
+
+// Unity plugin load event. This is a callback; a function that
+// unity calls when the plugin is loaded.
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+UnityPluginLoad(IUnityInterfaces * unityInterfaces)
+{
+    s_UnityInterfaces = unityInterfaces;
+    s_Graphics = unityInterfaces->Get<IUnityGraphics>();
+
+    s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
+
+    // Run OnGraphicsDeviceEvent(initialize) manually on plugin load
+    // to not miss the event in case the graphics device is already initialized
+    OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
+}
+
+// Unity plugin unload event
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+UnityPluginUnload()
+{
+    s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
+}
+
+
+extern "C" static void UNITY_INTERFACE_API OnRenderEvent(int eventId)
+{
+    Debug("Hello world!");
+}
+//This returns a function that unity can call whenever is needed
+UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API getEventFunction()
+{
+    return OnRenderEvent;
 }
